cd755c56f46252fc19cb4841ae3c856e511be64f
syed zohaib <zohaibsyed914@gmail.com>
2024-05-04 00:05:09 +0500

    working

---
+++ app.js
@@ -0,0 +1,140 @@
+// slide --50
+
+// let name = "zohaib"
+// let age = 22
+
+// let line1 = "my name is " + name + " my age is " + age + " year old"
+// let line2 = `my name is ${name} my age is ${age} year old`
+
+// console.log(line1)
+// console.log(line2)
+
+
+// slide --51
+
+// function fullname(firstName, lastName) {
+//     var result = firstName + " " + lastName
+//     return result.toUpperCase()
+// }
+
+// // fullname("syed", "zohaib")
+// console.log(fullname("syed", "zohaib"))
+
+// slide --52
+
+// let names = ['zohaib', "asmat", "arsalan", "haris", "sufiyan"]
+
+// console.log(names.length)
+// console.log(names[names.length - 1])
+
+// const lastName = ["qazi", "naveed", "dada"]
+// const allName = names.concat(lastName)
+// console.log(allName)
+
+// console.log(allName.reverse())
+
+// allName.unshift("inno")
+// allName.unshift("dodo")
+// console.log(allName)
+// allName.shift("dodo")
+// console.log(allName)
+
+// allName.push("kaka")
+// console.log(allName)
+
+// allName.pop("kara")
+// console.log(allName)
+
+// slide --53
+
+// completed
+
+// slide --54
+
+// let names = ['zohaib', "asmat", "arsalan", "haris", "sufiyan"]
+
+// const mainName = names.splice(2, 1)
+// console.log(mainName)
+// console.log(names)
+
+// slide --55
+
+// let namee = ["zohaib", "urooba", "bisma"]
+// const lastname = "naeem"
+// let newArray = []
+
+// for (var i = 0; i < namee.length; i++) {
+//     console.log(i)
+//     console.log(namee[i])
+//     newArray.push(namee[i])
+// }
+
+// console.log(namee)
+// console.log(newArray)
+
+// slide --56
+
+// let namee = ["zohaib", "urooba", "bisma"]
+// let lastName = "Naeem"
+// let newArray = []
+
+// for (let i = 0; i < namee.length; i++) {
+//     console.log(i)
+//     console.log(namee[i])
+//     let combine = `${namee[i]} ${lastName}`
+//     newArray.push(combine)
+// }
+// console.log(namee)
+// console.log(newArray)
+
+// slide --57
+
+// const gas = [20, 40, 60, 100]
+// const food = [100, 50]
+
+// function calculateTotal(arr) {
+//     let total = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         total += arr[i]
+
+//     }
+//     return total
+// }
+
+// const gasTotal = calculateTotal(gas)
+// const foodTotal = calculateTotal(food)
+// const randomTotal = calculateTotal([200, 80, 90, 40])
+
+
+// console.log({
+//     gas: gasTotal,
+//     food: foodTotal,
+//     random: randomTotal,
+// })
+
+// slide --58
+
+// const number = 1
+// const number2 = number
+// console.log(`the first value is ${number}`)
+// console.log(`the second value is ${number2}`)
+
+// slide --59
+
+// var number1 = 1
+// var number2 = number1
+// number2 = 7
+
+// console.log(`the number 1 is ${number1}`)
+// console.log(`the number 2 is ${number2}`)
+
+// slide --60
+
+// let person = { name: "kera" }
+// let person2 = person
+
+// console.log(`first person is ${person.name}`)
+// console.log(`second person is ${person2.name}`)
+
+
+
