e2119b0195db1d234be7695ba74bfee08dcc9015
Syed Zohaib <125762117+syedzohaib-dev@users.noreply.github.com>
2024-05-08 17:16:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,606 @@
+// slide --1
+
+// button
+
+// slide --2
+
+// document.write("hello world")
+// alert("hello world")
+// console.log("hello world")
+
+// slide --3
+
+// console.log("hello peolpe")
+
+// slide --4
+
+// var console: console;
+// console.log("heloo keree")
+
+// slide --5
+
+// let name = "john shirm tako v"
+// console.log(name)
+
+// slide --6
+
+// let full_Name = "Zohaib"
+// console.log(full_Name)
+
+// slide --7
+
+// var name = "kera"
+// let firstName = "baraKera"
+// const lastName = "chotaKera"
+// console.log(name)
+// console.log(firstName)
+// console.log(lastName)
+
+// slide --8
+
+// var firstName = "zohaib"
+// var lastName = "akhter"
+// var address = "main street"
+// address = "first street"
+// console.log(firstName)
+// console.log(lastName)
+// console.log(address)
+
+// slide --9
+
+// const name = "john's courses are the best"
+// console.log(name)
+
+
+// slide --10
+
+// const website = "youtube"
+// const url = 'https://www.' + website + ".com"
+// console.log(url)
+
+
+// slide --11
+
+// var street = "main street"
+// var country = "pakistan"
+// var fullMailingAddress = street + " " + country
+// console.log(fullMailingAddress)
+
+// slide --12 ...................
+
+// const number = 34
+// const number2 = 2.456
+// const number3 = "2.456"
+
+
+// const add = number + number2
+// const sub = number - number2
+// const mul = number * number2
+// const div = number / number2
+
+// console.log(add)
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
+// console.log(number3)
+
+// slide --13 ....................
+
+// let number = 40
+// number += 5
+// number += 5
+// number += 5
+// number += 5
+// number += 5
+// number -= 5
+// number++
+// number--
+// number--
+
+// console.log(number)
+
+// const slice = 10
+// const children = 4
+// const amount = slice % children
+// console.log(amount)
+
+// slide --14
+
+// const random = 4 + 6 + 10 * 10    //110
+// const random2 = (4 + 6 + 10) * 10    // 200
+// console.log(random)
+// console.log(random2)
+
+// slide --15
+
+// let score1 = 48
+// let score2 = 78
+// let score3 = 49
+
+// total = score1 + score2 + score3
+// console.log(total)
+
+// var plates = 20
+// var people = 7
+// plates = 21
+
+// var remainingPlates = plates / people
+
+// var massage = "There are " + plates + " plates avaible for " + people + " people"
+// console.log(massage)
+
+// slide --16
+
+// let name = "Zohaib"
+// let name2 = "Akhter"
+// let fullName = name + " " + name2
+// console.log(fullName)
+
+
+// let number = 3
+// let number2 = 7
+// let result = number + number2
+// console.log(result)
+
+
+// let value = name - name2
+// console.log(value)
+
+// let number3 = '10'
+// let number4 = '23'
+// let result2 = number3 - number4
+// console.log(result2)
+
+// slide --17
+// explain datatype
+
+// slide --18
+
+// let text = "some text"
+
+// let number = 45
+
+// let value1 = true
+// let value2 = false
+
+// const result = null
+
+// let name
+
+// console.log(typeof(text))
+// console.log(typeof(true))
+// console.log(typeof(number))
+// console.log(typeof(value1))
+// console.log(typeof(value2))
+// console.log(typeof(null))
+// console.log(typeof(name))
+
+// slide --19
+
+// const friend1 = "ana"
+// const friend2 = "ana"
+// const friend3 = "ana"
+// const friend4 = "ana"
+
+// const friend = ["john", "peter", "bob", "susy", 45, undefined, null]
+
+// let bestFriend = friend[2]
+// friend[4] = "anna"
+
+// console.log(friend)
+// console.log(friend[4])
+
+// console.log(bestFriend)
+// console.log(friend[3])
+
+// slide --20
+
+// let fruit = ["apple", "mango", "banana", "grapes", 45];
+// var firstFruit = fruit[0];
+// fruit.push("cherry");
+// console.log(fruit);
+// console.log(firstFruit);
+
+// //slide --21
+
+// console.log("heloo there Bob");
+// console.log("heloo there Anna");
+// console.log("heloo there Susy");
+
+// slide --22
+
+// function hello() {
+//     console.log("heloo there Bob");
+//     console.log("heloo there Anna");
+//     console.log("heloo there Susy");
+// }
+
+// hello()
+// hello()
+// hello()
+
+// slide --23
+
+// function greet(name) {
+//     console.log("Hello there " + name)
+// }
+// greet("Zohaib")
+// greet("Qazi")
+// greet("Naveed")
+
+// slide --24
+
+// const wallHeight = 80;
+// function calculate(value) {
+//     return value * 2.54;
+//     console.log('hello');
+// }
+// calculate(200)
+// const width = calculate(100);
+// const height = calculate(wallHeight);
+// const dimention = [width, height]
+// console.log(dimention)
+
+// slide --25 ____________
+
+// function calculateTotal(subtotal, tax) {
+//     return subtotal + tax
+// }
+// var order1 = calculateTotal(33, 44)
+// var order2 = calculateTotal(65, 27)
+// var order3 = calculateTotal(83, 84)
+
+// console.log(order1)
+// console.log(order2)
+// console.log(order3)
+
+
+// slide --25 ____________
+
+// function addValue(num1, num2) {
+//     return num1 + num2
+// }
+
+// const firstValue = addValue(33, 76)
+// const secondValue = addValue(54, 92)
+
+// const add = function (num1, num2) {
+//     return num1 + num2
+// }
+
+// const value = [firstValue, secondValue, add(5, 6)]
+// console.log(value)
+
+
+// slide --26
+
+
+// const person = {
+//     name: "zohaib",
+//     lastName: "Akhter",
+//     age: "22",
+//     education: "metric",
+//     married: false,
+//     sibbling: ["urooba", "bisma"],
+//     greeting: function () {
+//         console.log("hello my name is zohaib")
+//     },
+
+// }
+
+// const age = person.age
+// console.log(age)
+
+// console.log(person.name)
+// console.log(person.sibbling[1])
+// person.greeting()
+
+
+// slide --27
+
+// let car = {
+//     make: "toyota",
+//     model: "corola",
+//     year: 2024,
+//     color: ["white", "black", "silver"],
+//     hybrid: true,
+//     drive: "yes",
+//     stop: "no",
+//     bodySetup: function () {
+//         console.log("A car body is too good and front look like a bad boy")
+//     }
+// }
+
+// console.log(car.make)
+// console.log(car.color[0])
+// console.log(car.drive)
+
+// slide --28
+
+// const value = 2 > 1
+// console.log(typeof (value))
+
+// const value2 = 1 > 2
+// if (value2) {
+//     console.log("hello world")
+// } else {
+//     console.log("hello people")
+// }
+
+// slide --29
+
+// const num1 = 6
+// const num2 = 6
+// const result = num1 >= num2
+// if (num1 > num2) {
+//     console.log("num1 is bigger then num2")
+// } else if (result) {
+//     console.log("num1 and num2 is equal")
+// } else {
+//     console.log("second number is bigger then first")
+// }
+
+// slide --30
+
+// const num1 = 6
+// const num2 = 6
+// const result = num1 >= num2
+
+// const value = false
+
+// if (!value) {
+//     console.log("value is true")
+// }
+
+// slide --31
+
+// const namee = "Zohaib"
+// const age = 22
+
+// if (namee === "ali" || age === 22) {
+//     console.log("Hello there user")
+// } else {
+//     console.log("wrong value")
+// }
+
+// slide --32
+
+// const name = "bob"
+// const age = 24
+
+// if (name === "bob" && age === 24) {
+//     console.log("hello user")
+// } else {
+//     console.log("user invalide")
+// }
+
+// slide --33
+
+// const dice = 0
+
+// if (dice === 1) {
+//     console.log("you got 1")
+// } else if (dice === 2) {
+//     console.log("you got 2")
+// } else {
+//     console.log("you did not roll the Dice")
+// }
+
+// slide --34
+
+// let dice = 0
+
+// if (dice === 1) {
+//     console.log("You got 1")
+// } else if (dice === 2) {
+//     console.log("YOu got 2")
+// } else if (dice < 1 || dice > 6) {
+//     console.log("you did not roll the dice")
+// } else {
+//     console.log("error")
+// }
+
+// slide --35
+
+// const person1 = {
+//     name: "qazi",
+//     age: 33,
+//     status: "resident"
+// }
+// if (person1.age > 18 && person1.status === "resident") {
+//     console.log("you are a pakistani boy " + person1.name)
+// }
+
+
+// const person2 = {
+//     name: "naveed",
+//     age: 21,
+//     status: "tourist"
+// }
+// if (person2.age > 18 && person2.status === "resident") {
+//     console.log("you are a pakistani box")
+// } else {
+//     console.log("you are not pakistani boy " + person2.name)
+// }
+
+// slide --35
+
+// var key = 1
+// switch (key) {
+//     case 1:
+//         console.log("you got 1")
+//         break;
+//     case 2:
+//         console.log("you got 2")
+//         break;
+//     case 3:
+//         console.log("you got 3")
+//         break;
+//     default:
+//         console.log("You did not roll the dice")
+// }
+
+// slide --36
+
+// let amount = 10
+
+// while (amount > 0) {
+//     console.log("I have " + amount + " dollar and I am going to the mall")
+//     amount--
+// }
+
+// slide --37
+
+// let money = 0
+
+// do {
+//     console.log("You have " + money + " Dollar")
+//     money++
+// } while (money < 10)
+
+// slide --38
+
+// let money = 12;
+// do{
+//     console.log("you habe " + money + " dollar")
+//     money++
+// }while(money < 10)
+
+// slide --39
+
+// for (var number = 0; number < 10; number++) {
+//     console.log("And the number is " + number)
+// }
+
+// slide --40
+
+// const person = {
+//     name:"peter",
+//     greeting(){
+//         console.log("hey i am peter")
+//     }
+// }
+
+// console.log(person)
+// console.log(person.name)
+// person.greeting()
+
+// silde --41
+
+// const name = "peter jorden"
+// const result = name.length
+// console.log(result)
+
+// slide --42
+
+// let text = " Peter Jordan"
+// let result = text.length
+// console.log(result)
+
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.charAt(0))
+
+// slide --43
+
+// same as 42
+
+
+// slide --44
+
+// let text = " Peter Jordan"
+// let result = text.length
+// console.log(result)
+
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.charAt(0))
+// console.log(text.charAt(12))
+
+// slide --45
+
+// let text = " Peter Jordan"
+// let result = text.length
+// console.log(result)
+
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.charAt(0))
+// console.log(text.charAt(text.length - 1))
+// console.log(text.indexOf("p"))
+
+// slide --46
+
+// let text = " Peter Jordan"
+// let result = text.length
+// console.log(result)
+
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.charAt(0))
+// console.log(text.charAt(text.length - 1))
+// console.log(text.indexOf("e"))
+
+
+// slide --47
+
+// let text = " Peter Jordan"
+// let result = text.length
+// console.log(result)
+
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.charAt(0))
+// console.log(text.charAt(text.length - 1))
+// console.log(text.indexOf("e"))
+// console.log(text)
+// console.log(text.trim())
+// console.log(text.startsWith("Peter"))
+
+// slide --48
+
+// let text = " Peter Jordan"
+// let result = text.length
+// console.log(result)
+
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.charAt(0))
+// console.log(text.charAt(text.length - 1))
+// console.log(text.indexOf("e"))
+// console.log(text)
+// console.log(text.trim())
+// console.log(text.startsWith(" Peter"))
+
+// slide --49
+
+
+// let text = " Peter Jordan"
+// let result = text.length
+// console.log(result)
+
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.charAt(0))
+// console.log(text.charAt(text.length - 1))
+// console.log(text.indexOf("e"))
+// console.log(text)
+// console.log(text.trim())
+// console.log(text.trim().toLowerCase().startsWith("peter"))
+// console.log(text.includes("eter"))
+// console.log(text.slice(0, 2))
+// console.log(text.slice(-3))
+
+// slide --50
+
+
 // slide --50
 
 // let name = "zohaib"
